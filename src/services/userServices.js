f56fcd94145482a3e5a190067a1bec6d11b4904f
Patrick Riley <priley86@gmail.com>
2019-07-24 17:29:00 -0400

    feat(locale): use cookie to determine current locale
    * use rh_locale cookie to set i18n locale if it's available
    * use env variables as a fallback if we can't find a supported locale

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,3 +1,4 @@
+import Cookie from 'js-cookie';
 import { helpers } from '../common/helpers';
 
 const authorizeUser = () => {
@@ -14,19 +15,37 @@ const authorizeUser = () => {
   return returnMethod;
 };
 
+const getLocaleFromCookie = () => {
+  try {
+    const supportedLocales = process.env.REACT_APP_CONFIG_SERVICE_SUPPORTED_LOCALES.split(',');
+    const cookieValue = Cookie.get(process.env.REACT_APP_CONFIG_SERVICE_LOCALES_COOKIE);
+    if (cookieValue && supportedLocales.indexOf(cookieValue) > -1) {
+      const majorLocaleValue = cookieValue.split('_')[0];
+      return { value: majorLocaleValue, key: cookieValue };
+    }
+    return null;
+  } catch (e) {
+    if (!helpers.TEST_MODE) {
+      console.warn(`Unable to parse locale ${e.message}`);
+    }
+    return null;
+  }
+};
+
 const getLocale = () => {
-  const locale = {
+  const defaultLocale = {
     value: process.env.REACT_APP_CONFIG_SERVICE_LOCALES_DEFAULT_LNG,
-    key: process.env.REACT_APP_CONFIG_SERVICE_LOCALES_DEFAULT_LNG_DESC
+    key: process.env.REACT_APP_CONFIG_SERVICE_LOCALES_DEFAULT_LNG_KEY
   };
+  const cookieLocale = getLocaleFromCookie();
+  const locale = cookieLocale || defaultLocale;
 
   return new Promise(resolve => {
     if (locale) {
       return resolve({
-        data: locale
+        data: getLocaleFromCookie() || defaultLocale
       });
     }
-
     return resolve({});
   });
 };
