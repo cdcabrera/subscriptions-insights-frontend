1a020d7c4146c2a8ec18d96909c48871e32fec57
CD Cabrera <ccabrera@redhat.com>
2019-08-16 09:58:58 -0400

    DEV stash

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 #
 #
+# Update repo with prod branches
+#
+branches()
+{
+  local CI_BRANCH=$1
+  local CI_COMMIT=$2
+
+  if [[ $CI_BRANCH != "master" ]] || [[ -z "$(${CI_COMMIT} | grep "chore(release):")" ]]; then
+    return 0
+  fi
+
+  echo "Creating production release branches... "
+
+  # git config, add remote, etc...
+
+  git push -f --quiet origin master:prod-beta
+  git push -f --quiet origin master:prod-stable
+
+  echo "SUCCESS"
+}
+#
+#
+# Clean up build
+#
+clean()
+{
+  echo "Cleaning up build resources..."
+  rm ./build/*manifest*.js*
+  rm ./build/service-worker.js
+}
+#
+#
 # main()
 #
 {
-  echo "Cleaning up build resources..."
+  clean
 
-  # clean up build
-  rm ./build/*manifest*.js*
-  rm ./build/service-worker.js
+  # see .travis.yml globals
+  branches ${BRANCH} ${COMMIT_MESSAGE}
 }
