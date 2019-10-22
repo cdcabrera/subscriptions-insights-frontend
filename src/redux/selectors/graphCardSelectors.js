09969a3b838ca1022245e7509c2d0fa8573258e2
CD Cabrera <ccabrera@redhat.com>
2019-10-22 12:56:08 -0400

    DEV test

--- src/redux/selectors/graphCardSelectors.js
+++ src/redux/selectors/graphCardSelectors.js
@@ -1,6 +1,9 @@
 import { createSelector } from 'reselect';
+import moment from 'moment';
 import _get from 'lodash/get';
 import { rhelApiTypes } from '../../types/rhelApiTypes';
+import { graphHelpers } from '../../common';
+import { translate } from '../../components/i18n/i18n';
 
 const rhelGraphCardCache = {};
 
@@ -10,8 +13,11 @@ const rhelGraphCardSelector = createSelector(
   [rhelGraph],
   rhelGraphReducer => {
     const { capacity = {}, report = {} } = rhelGraphReducer || {};
-    const reportGranularity = _get(report, ['metaQuery', rhelApiTypes.RHSM_API_QUERY_GRANULARITY]);
-    const capacityGranularity = _get(capacity, ['metaQuery', rhelApiTypes.RHSM_API_QUERY_GRANULARITY]);
+    const reportMetaQuery = _get(report, ['metaQuery'], {});
+    const capacityMetaQuery = _get(capacity, ['metaQuery'], {});
+
+    const reportGranularity = _get(reportMetaQuery, [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]);
+    const capacityGranularity = _get(capacityMetaQuery, [rhelApiTypes.RHSM_API_QUERY_GRANULARITY]);
     const granularity = reportGranularity || capacityGranularity || null;
     const cachedGranularity = (granularity && rhelGraphCardCache[granularity]) || {};
     const initialLoad = typeof cachedGranularity.initialLoad === 'boolean' ? cachedGranularity.initialLoad : true;
@@ -21,10 +27,7 @@ const rhelGraphCardSelector = createSelector(
       fulfilled: false,
       pending: false,
       initialLoad,
-      graphData: {
-        usage: [],
-        capacity: []
-      },
+      graphData: [],
       ...cachedGranularity
     };
 
@@ -39,8 +42,49 @@ const rhelGraphCardSelector = createSelector(
     }
 
     if (capacity.fulfilled && report.fulfilled && granularity) {
-      updatedData.graphData.usage = _get(report, ['data', rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA], []);
-      updatedData.graphData.capacity = _get(capacity, ['data', rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA], []);
+      const products = _get(report, ['data', rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA], []);
+      const threshold = _get(capacity, ['data', rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA], []);
+      const aggregatedData = [];
+
+      products.forEach((value, index) => {
+        if (value) {
+          const stringDate = moment
+            .utc(value[rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.DATE])
+            .startOf('day')
+            .toISOString();
+
+          /**
+           * FixMe: per API the list indexes should match on capacity and reporting.
+           * Once reasonable testing has occurred consider removing this check.
+           */
+          const checkThresholdDate = dataThresholdItem => {
+            if (!dataThresholdItem) {
+              return false;
+            }
+
+            const stringThresholdDate = moment
+              .utc(dataThresholdItem[rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.DATE])
+              .startOf('day')
+              .toISOString();
+
+            return moment(stringDate).isSame(stringThresholdDate);
+          };
+
+          aggregatedData.push({
+            date: value[rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.DATE],
+            sockets: Number.parseInt(value[rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.SOCKETS], 10),
+            hypervisor: Number.parseInt(value[rhelApiTypes.RHSM_API_RESPONSE_PRODUCTS_DATA_TYPES.HYPERVISOR], 10),
+            socketsThreshold: Number.parseInt(
+              (checkThresholdDate(threshold && threshold[index]) &&
+                threshold[index][rhelApiTypes.RHSM_API_RESPONSE_CAPACITY_DATA_TYPES.SOCKETS]) ||
+                0,
+              10
+            )
+          });
+        }
+      });
+
+      updatedData.graphData = aggregatedData;
       updatedData.initialLoad = false;
       updatedData.fulfilled = true;
 
