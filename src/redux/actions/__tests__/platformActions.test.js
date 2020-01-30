3747fd3be0921bbdc56078b700ab6516d8d3ab3f
CD Cabrera <ccabrera@redhat.com>
2020-01-29 20:00:03 -0500

    fix(platformServices,userServices): issues/43 centralize globals
    * build, lint promise reject errors
    * build, setupTests, apply platform global mock
    * platformServices, restructure getUser, navigation methods
    * userServices, restructure authorizeUser
    * config, use platformService instead of emulated response
    * platformActions, allow nav methods to return unlistener responses

--- src/redux/actions/__tests__/platformActions.test.js
+++ src/redux/actions/__tests__/platformActions.test.js
@@ -1,32 +1,19 @@
 import { platformActions } from '../platformActions';
-import { helpers } from '../../../common';
 
 describe('PlatformActions', () => {
-  beforeAll(() => {
-    window.insights = {
-      chrome: {
-        auth: helpers.noop,
-        identifyApp: helpers.noop,
-        init: helpers.noop,
-        navigation: helpers.noop,
-        on: helpers.noop
-      }
-    };
-  });
-
   it('Should return a dispatch object for the initializeChrome method', () => {
     expect(platformActions.initializeChrome()).toMatchSnapshot('dispatch object');
   });
 
-  it('Should return a dispatch object for the onNavigation method', () => {
-    expect(platformActions.onNavigation()).toMatchSnapshot('dispatch object');
+  it('Should return a function for the onNavigation method', () => {
+    expect(platformActions.onNavigation()).toMatchSnapshot('function');
   });
 
   it('Should return a dispatch object for the setAppName method', () => {
     expect(platformActions.setAppName()).toMatchSnapshot('dispatch object');
   });
 
-  it('Should return a dispatch object for the setNavigation method', () => {
-    expect(platformActions.setNavigation()).toMatchSnapshot('dispatch object');
+  it('Should return a function for the setNavigation method', () => {
+    expect(platformActions.setNavigation()).toMatchSnapshot('function');
   });
 });
