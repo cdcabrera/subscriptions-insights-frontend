f7768548a671e20e1580f70d146281e76332c736
CD Cabrera <ccabrera@redhat.com>
2020-07-31 20:32:19 -0400

    DEV expand toolbar test

--- src/components/toolbar/__tests__/toolbar.test.js
+++ src/components/toolbar/__tests__/toolbar.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { Toolbar } from '../toolbar';
 import { toolbarTypes } from '../toolbarTypes';
+import { RHSM_API_QUERY_SLA, RHSM_API_QUERY_USAGE } from '../../../types/rhsmApiTypes';
 
 describe('Toolbar Component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render a non-connected component', () => {
     const props = {};
     const component = shallow(<Toolbar {...props} />);
@@ -11,19 +16,40 @@ describe('Toolbar Component', () => {
     expect(component).toMatchSnapshot('non-connected');
   });
 
-  it('should handle updating state and dispatching an sla filter', () => {
+  it('should handle updating state and dispatching a sla filter', () => {
     const props = {};
 
-    const dispatchFilter = jest.spyOn(Toolbar.prototype, 'dispatchFilter');
+    const dispatchFilter = jest.spyOn(Toolbar.prototype, 'setDispatchFilter');
     const component = mount(<Toolbar {...props} />);
+    component.setState({ filterCategory: RHSM_API_QUERY_SLA });
+
     const componentInstance = component.instance();
 
-    const slaOption = toolbarTypes.getOptions('sla').options[0];
-    componentInstance.onSlaSelect({ selected: { ...slaOption }, value: slaOption.value });
-    expect(componentInstance.state).toMatchSnapshot('sla selected');
+    const option = toolbarTypes.getOptions(RHSM_API_QUERY_SLA).options[0];
+    componentInstance.onSlaSelect({ selected: { ...option }, value: option.value });
+    expect(componentInstance.state).toMatchSnapshot(`${RHSM_API_QUERY_SLA} selected`);
 
     componentInstance.onClear();
-    expect(componentInstance.state).toMatchSnapshot('clear filters');
+    expect(componentInstance.state).toMatchSnapshot(`${RHSM_API_QUERY_SLA} clear filters`);
+
+    expect(dispatchFilter).toHaveBeenCalledTimes(2);
+  });
+
+  it('should handle updating state and dispatching a usage filter', () => {
+    const props = {};
+
+    const dispatchFilter = jest.spyOn(Toolbar.prototype, 'setDispatchFilter');
+    const component = mount(<Toolbar {...props} />);
+    component.setState({ filterCategory: RHSM_API_QUERY_USAGE });
+
+    const componentInstance = component.instance();
+
+    const option = toolbarTypes.getOptions(RHSM_API_QUERY_USAGE).options[0];
+    componentInstance.onUsageSelect({ selected: { ...option }, value: option.value });
+    expect(componentInstance.state).toMatchSnapshot(`${RHSM_API_QUERY_USAGE} selected`);
+
+    componentInstance.onClear();
+    expect(componentInstance.state).toMatchSnapshot(`${RHSM_API_QUERY_USAGE} clear filters`);
 
     expect(dispatchFilter).toHaveBeenCalledTimes(2);
   });
