1e8a94bf56546b2f9dea5baee70c09e97b3c8ac5
CD Cabrera <ccabrera@redhat.com>
2021-02-23 01:04:19 -0500

    feat(toolbarFieldRangeGranularity): ent-3507 month ranges
    * toolbarFieldRangeGranularity, display months, associated date ranges
    * select, maxHeight pf prop

--- src/common/dateHelpers.js
+++ src/common/dateHelpers.js
@@ -1,6 +1,7 @@
 import moment from 'moment/moment';
 import { helpers } from './helpers';
 import { RHSM_API_QUERY_GRANULARITY_TYPES as GRANULARITY_TYPES } from '../types/rhsmApiTypes';
+import { translate } from '../components/i18n/i18n';
 
 /**
  * Return a date.
@@ -17,11 +18,12 @@ const getCurrentDate = () =>
  * @param {Date} params.date Start date, typically the current date.
  * @param {number} params.subtract Number of granularity type to subtract from the current date.
  * @param {string} params.measurement Granularity type.
+ * @param {string} params.endOfMeasurement Granularity type.
  * @returns {{endDate: Date, startDate: Date}}
  */
-const setRangedDateTime = ({ date, subtract, measurement }) => ({
+const setRangedDateTime = ({ date, subtract, measurement, endOfMeasurement = 'days' }) => ({
   startDate: moment.utc(date).startOf(measurement).subtract(subtract, measurement).toDate(),
-  endDate: moment.utc(date).startOf(measurement).endOf('days').toDate()
+  endDate: moment.utc(date).startOf(measurement).endOf(endOfMeasurement).toDate()
 });
 
 const currentDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 1, measurement: 'days' });
@@ -29,6 +31,12 @@ const defaultDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 30
 const weeklyDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 12, measurement: 'weeks' });
 const monthlyDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 12, measurement: 'months' });
 const quarterlyDateTime = setRangedDateTime({ date: getCurrentDate(), subtract: 36, measurement: 'months' });
+const rangedDateTime = setRangedDateTime({
+  date: getCurrentDate(),
+  subtract: 11,
+  measurement: 'months',
+  endOfMeasurement: 'months'
+});
 
 /**
  * Return a range of time based on known granularity types.
@@ -50,6 +58,79 @@ const getRangedDateTime = granularity => {
     default:
       return { ...defaultDateTime };
   }
+};
+
+const getMonthlyRangedDateTime = month => {
+  const { startDate, endDate } = { ...rangedDateTime };
+  const keyDateTimeRanges = {};
+  let listDateTimeRanges = [];
+
+  const startDateUpdated = moment.utc(startDate);
+  const endDateUpdated = moment.utc(endDate);
+
+  while (endDateUpdated > startDateUpdated || startDateUpdated.format('M') === endDateUpdated.format('M')) {
+    const dateTime = {
+      value: {
+        startDate: startDateUpdated.toDate()
+      }
+    };
+
+    const startDateYear = Number.parseInt(startDateUpdated.year(), 10);
+    let priorYear = listDateTimeRanges?.[listDateTimeRanges.length - 1]?.value.startDate;
+
+    priorYear =
+      priorYear &&
+      Number.parseInt(moment.utc(listDateTimeRanges?.[listDateTimeRanges.length - 1]?.value.startDate).year(), 10);
+
+    const isNewYear = (priorYear && startDateYear !== priorYear) || false;
+    const titleYear = startDateUpdated.format('MMMM YYYY');
+    const title = startDateUpdated.format('MMMM');
+
+    startDateUpdated.add(1, 'month');
+
+    dateTime.title = (isNewYear && titleYear) || title;
+    dateTime.value.endDate = startDateUpdated.startOf('month').toDate();
+
+    dateTime.title = translate('curiosity-toolbar.granularityRange', { context: dateTime.title });
+    keyDateTimeRanges[startDateUpdated.format('MMMM')] = dateTime;
+    listDateTimeRanges.push(dateTime);
+  }
+  /*
+  while (endDateUpdated > startDateUpdated || startDateUpdated.format('M') === endDateUpdated.format('M')) {
+    const dateTime = {
+      value: {
+        startDate: startDateUpdated.format()
+      }
+    };
+
+    const titleYear = startDateUpdated.format('MMMM YYYY');
+    const title = startDateUpdated.format('MMMM');
+    const startDateYear = Number.parseInt(startDateUpdated.year(), 10);
+
+    startDateUpdated.add(1, 'month');
+    const nextDateYear = Number.parseInt(startDateUpdated.year(), 10);
+    const isNewYear = startDateYear !== nextDateYear;
+
+    dateTime.title = (isNewYear && titleYear) || title;
+    dateTime.value.endDate = startDateUpdated.startOf('month').format();
+
+    // dateTime.title = translate('curiosity-toolbar.granularityRange', { context: dateTime.title });
+    keyDateTimeRanges[startDateUpdated.format('MMMM')] = dateTime;
+    listDateTimeRanges.push(dateTime);
+  }
+   */
+
+  if (keyDateTimeRanges?.month) {
+    return keyDateTimeRanges[month];
+  }
+
+  listDateTimeRanges = listDateTimeRanges.reverse();
+  listDateTimeRanges[0] = {
+    ...listDateTimeRanges[0],
+    title: translate('curiosity-toolbar.granularityRange', { context: 'current' })
+  };
+
+  return { keyDateTimeRanges, listDateTimeRanges };
 };
 
 /**
@@ -87,6 +168,7 @@ const timestampQuarterFormats = {
 
 const dateHelpers = {
   getCurrentDate,
+  getMonthlyRangedDateTime,
   getRangedDateTime,
   setRangedDateTime,
   currentDateTime,
@@ -102,6 +184,7 @@ const dateHelpers = {
 export {
   dateHelpers as default,
   getCurrentDate,
+  getMonthlyRangedDateTime,
   getRangedDateTime,
   setRangedDateTime,
   currentDateTime,
