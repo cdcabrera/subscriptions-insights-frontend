d61b43e5d819e6244f10a379b6ae3f49bf4ece24
CD Cabrera <ccabrera@redhat.com>
2021-03-26 09:36:27 -0400

    DEV 001 rollup package test

---
+++ packages/curiosity-charts-rollup/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { rhsmServices } from '../../../src/services/rhsmServices';
+
+const CuriosityCharts = () => {
+  // const count = Object.entries(rhsmServices).length;
+
+  return (
+    <div>
+      <h2>Welcome to React components {count}</h2>
+    </div>
+  );
+};
+
+export { CuriosityCharts as default, CuriosityCharts };
