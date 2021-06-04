8efc97fe83f83c36ecf5b3e45caa64dbba2f00cd
CD Cabrera <ccabrera@redhat.com>
2021-06-04 01:47:01 -0400

    DEV simplify redirect

--- src/components/router/redirect.js
+++ src/components/router/redirect.js
@@ -11,10 +11,11 @@ import { helpers } from '../../common';
  * @param {string} props.baseName
  * @param {boolean} props.isForced
  * @param {string} props.route
+ * @param {string} props.routes
  * @param {string} props.url
  * @returns {Node}
  */
-const Redirect = ({ baseName, isForced, route, url }) => {
+const Redirect = ({ baseName, isForced, route, routes, url }) => {
   /**
    * Bypass router, force the location.
    */
@@ -27,14 +28,13 @@ const Redirect = ({ baseName, isForced, route, url }) => {
     }
   };
 
-  if (!isForced && route) {
-    const routeDetail = routerHelpers.getRouteConfigByPath({ pathName: route }).firstMatch;
-    return <Router routes={[{ ...routeDetail, path: '*' }, ...routerHelpers.routes]} />;
+  const matchedRoute = route && routerHelpers.getRouteConfig({ pathName: route, id: route });
+
+  if (!isForced && matchedRoute) {
+    return <Router routes={[{ ...matchedRoute, path: '*' }, ...routes]} />;
   }
 
-  if (url || route) {
-    forceNavigation();
-  }
+  forceNavigation();
 
   return (helpers.TEST_MODE && <React.Fragment>Redirected towards {url || route}</React.Fragment>) || null;
 };
@@ -42,26 +42,28 @@ const Redirect = ({ baseName, isForced, route, url }) => {
 /**
  * Prop types.
  *
- * @type {{isRedirect: boolean, route: string, isReplace: boolean, baseName: string, url: string,
+ * @type {{isRedirect: boolean, route: string, routes: Array, isReplace: boolean, baseName: string, url: string,
  *    isForced: boolean}}
  */
 Redirect.propTypes = {
   baseName: PropTypes.string,
   isForced: PropTypes.bool,
   route: PropTypes.string,
+  routes: PropTypes.array,
   url: PropTypes.string
 };
 
 /**
  * Default props.
  *
- * @type {{isRedirect: boolean, route: string, isReplace: boolean, baseName: string, url: string,
+ * @type {{isRedirect: boolean, route: string, routes: Array, isReplace: boolean, baseName: string, url: string,
  *    isForced: boolean}}
  */
 Redirect.defaultProps = {
   baseName: routerHelpers.baseName,
   isForced: false,
   route: null,
+  routes: routerHelpers.routes,
   url: null
 };
 
