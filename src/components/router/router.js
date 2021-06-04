29c2e7098067f57deb5b48d9549f9d224634e0c0
CD Cabrera <ccabrera@redhat.com>
2021-06-03 23:44:12 -0400

    DEV reset router

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect as ReactRouterDomRedirect, Route, Switch } from 'react-router-dom';
-// import { useMount } from 'react-use';
+import { useMount } from 'react-use';
 import { routerHelpers } from './routerHelpers';
 import { Loader } from '../loader/loader';
 
@@ -19,74 +19,67 @@ const Router = ({ routes } = {}) => {
   /**
    * Initialize routes.
    */
-  useEffect(() => {
-    const setupRoutes = async () => {
-      const activateOnErrorRoute = routes.find(route => route.activateOnError === true);
+  useMount(async () => {
+    const activateOnErrorRoute = routes.find(route => route.activateOnError === true);
 
-      const results = await Promise.all(
-        routes.map(async item => {
-          if (item.disabled) {
-            return null;
-          }
+    const results = await Promise.all(
+      routes.map(async item => {
+        if (item.disabled) {
+          return null;
+        }
 
-          const View =
-            (React.isValidElement(item.component) && item.component) ||
-            (await routerHelpers.importView(item.component));
+        const View =
+          (React.isValidElement(item.component) && item.component) || (await routerHelpers.importView(item.component));
 
-          return (
-            <Route
-              exact={item.exact}
-              key={item.path}
-              path={item.path}
-              strict={item.strict}
-              render={({ location, ...routeProps }) => {
-                const routeConfig = item.id && routerHelpers.getRouteConfig({ id: item.id });
-                const { URLSearchParams, decodeURIComponent } = window;
-                const parsedSearch = {};
+        return (
+          <Route
+            exact={item.exact}
+            key={item.path}
+            path={item.path}
+            strict={item.strict}
+            render={({ location, ...routeProps }) => {
+              const routeConfig = item.id && routerHelpers.getRouteConfig({ id: item.id });
+              const { URLSearchParams, decodeURIComponent } = window;
+              const parsedSearch = {};
 
-                [
-                  ...new Set(
-                    [...new URLSearchParams(decodeURIComponent(location.search))].map(
-                      ([param, value]) => `${param}~${value}`
-                    )
+              [
+                ...new Set(
+                  [...new URLSearchParams(decodeURIComponent(location.search))].map(
+                    ([param, value]) => `${param}~${value}`
                   )
-                ].forEach(v => {
-                  const [param, value] = v.split('~');
-                  parsedSearch[param] = value;
-                });
+                )
+              ].forEach(v => {
+                const [param, value] = v.split('~');
+                parsedSearch[param] = value;
+              });
 
-                const updatedLocation = {
-                  ...location,
-                  parsedSearch
-                };
+              const updatedLocation = {
+                ...location,
+                parsedSearch
+              };
 
-                return (
-                  <View
-                    routeDetail={{
-                      baseName: routerHelpers.baseName,
-                      errorRoute: activateOnErrorRoute,
-                      routes,
-                      routeItem: { ...item },
-                      ...routeConfig
-                    }}
-                    location={updatedLocation}
-                    {...routeProps}
-                  />
-                );
-              }}
-            />
-          );
-        })
-      );
+              return (
+                <View
+                  routeDetail={{
+                    baseName: routerHelpers.baseName,
+                    errorRoute: activateOnErrorRoute,
+                    routes,
+                    routeItem: { ...item },
+                    ...routeConfig
+                  }}
+                  location={updatedLocation}
+                  {...routeProps}
+                />
+              );
+            }}
+          />
+        );
+      })
+    );
 
-      setUpdatedRoutes(results);
-      setRedirectRoot(routes.find(({ disabled, redirect }) => !disabled && redirect) ?? null);
-    };
-
-    console.log('HEY >>>', routes);
-
-    setupRoutes();
-  }, [routes]);
+    setUpdatedRoutes(results);
+    setRedirectRoot(routes.find(({ disabled, redirect }) => !disabled && redirect) ?? null);
+  });
 
   return (
     <React.Suspense fallback={<Loader variant="title" />}>
