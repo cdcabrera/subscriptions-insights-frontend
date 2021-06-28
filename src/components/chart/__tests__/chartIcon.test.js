ea3edffe9d32440b4d2a106aba1e8039c8163f9c
CD Cabrera <ccabrera@redhat.com>
2021-06-28 17:51:05 -0400

    DEV chartIcon test update

--- src/components/chart/__tests__/chartIcon.test.js
+++ src/components/chart/__tests__/chartIcon.test.js
@@ -12,6 +12,7 @@ describe('ChartIcon Component', () => {
 
   it('should handle basic icons, variations in settings', async () => {
     const props = {
+      size: 'md',
       symbol: 'eye',
       title: 'lorem ipsum eye'
     };
@@ -19,12 +20,14 @@ describe('ChartIcon Component', () => {
     expect(component).toMatchSnapshot('eye icon');
 
     component.setProps({
+      size: 'lg',
       symbol: 'dash',
       title: 'lorem ipsum dash'
     });
     expect(component).toMatchSnapshot('dash icon');
 
     component.setProps({
+      size: 'xl',
       symbol: 'threshold',
       title: null,
       fill: '#ff0000'
