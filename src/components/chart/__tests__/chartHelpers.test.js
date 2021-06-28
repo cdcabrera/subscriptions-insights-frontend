47b0c04f085d09bdc6a85d19874d7c6ac40a8d3a
CD Cabrera <ccabrera@redhat.com>
2021-06-28 16:31:32 -0400

    DEV chartHelpers tests

--- src/components/chart/__tests__/chartHelpers.test.js
+++ src/components/chart/__tests__/chartHelpers.test.js
@@ -65,4 +65,97 @@ describe('ChartHelpers', () => {
     };
     expect(chartHelpers.generateDomains(options)).toMatchSnapshot('domain, maxY object');
   });
+
+  it('should generate element props', () => {
+    const options = {
+      dataSets: [
+        {
+          id: 'lorem',
+          data: [
+            { x: 0, y: 10 },
+            { x: 1, y: 10 },
+            { x: 2, y: 10 },
+            { x: 3, y: 10 },
+            { x: 4, y: 10 },
+            { x: 5, y: 10 }
+          ]
+        },
+        {
+          id: 'ipsum',
+          isStacked: true,
+          data: [
+            { x: 0, y: 20 },
+            { x: 1, y: 10 },
+            { x: 2, y: 30 },
+            { x: 3, y: 50 },
+            { x: 4, y: 100 },
+            { x: 5, y: 10 }
+          ]
+        },
+        {
+          id: 'dolor',
+          isStacked: true,
+          data: [
+            { x: 0, y: 20 },
+            { x: 1, y: 10 },
+            { x: 2, y: 30 },
+            { x: 3, y: 50 },
+            { x: 4, y: 100 },
+            { x: 5, y: 10 }
+          ]
+        }
+      ],
+      maxX: 6,
+      maxY: 200
+    };
+
+    expect(chartHelpers.generateElementsProps(options)).toMatchSnapshot('props');
+  });
+
+  it('should generate tooltip data/content', () => {
+    const mockContent = jest.fn();
+    const options = {
+      content: mockContent,
+      dataSets: [
+        {
+          id: 'lorem',
+          data: [
+            { x: 0, y: 10 },
+            { x: 1, y: 10 },
+            { x: 2, y: 10 },
+            { x: 3, y: 10 },
+            { x: 4, y: 10 },
+            { x: 5, y: 10 }
+          ]
+        },
+        {
+          id: 'ipsum',
+          isStacked: true,
+          data: [
+            { x: 0, y: 20 },
+            { x: 1, y: 10 },
+            { x: 2, y: 30 },
+            { x: 3, y: 50 },
+            { x: 4, y: 100 },
+            { x: 5, y: 10 }
+          ]
+        },
+        {
+          id: 'dolor',
+          isStacked: true,
+          data: [
+            { x: 0, y: 20 },
+            { x: 1, y: 10 },
+            { x: 2, y: 30 },
+            { x: 3, y: 50 },
+            { x: 4, y: 100 },
+            { x: 5, y: 10 }
+          ]
+        }
+      ]
+    };
+
+    expect(chartHelpers.generateTooltipData(options)).toMatchSnapshot('tooltip, settings');
+    expect(mockContent.mock.calls).toMatchSnapshot('tooltip, content');
+  });
 });
