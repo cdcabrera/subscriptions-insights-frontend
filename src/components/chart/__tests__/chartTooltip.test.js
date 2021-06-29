1bcbe76ef4d60957ea9824396fd14af3fadb8bc2
CD Cabrera <ccabrera@redhat.com>
2021-06-29 11:39:47 -0400

    DEV chartTooltip

--- src/components/chart/__tests__/chartTooltip.test.js
+++ src/components/chart/__tests__/chartTooltip.test.js
@@ -30,6 +30,33 @@ describe('ChartTooltip Component', () => {
     expect(updatedComponent).toMatchSnapshot('basic');
   });
 
+  it('should return an empty tooltip when missing content', () => {
+    const settings = {
+      chartSettings: {
+        padding: {},
+        tooltipDataSetLookUp: {
+          1: {
+            tooltip: undefined
+          },
+          2: {
+            tooltip: undefined
+          }
+        }
+      }
+    };
+
+    const props = {
+      x: 2,
+      y: 3,
+      datum: { x: 1 }
+    };
+
+    const Component = chartTooltip(settings);
+    const updatedComponent = shallow(<Component {...props} />);
+
+    expect(updatedComponent).toMatchSnapshot('missing content');
+  });
+
   it('should handle tooltip position', () => {
     const settings = {
       chartContainerRef: () => ({
