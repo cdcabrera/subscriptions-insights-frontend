d6fd490a747ea6f978f0f95bea3d4f3de60327c4
CD Cabrera <ccabrera@redhat.com>
2021-12-02 13:30:50 -0500

    DEV eslint, babel for dotenv

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -2,9 +2,7 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { setHtmlPlugin, setReplacePlugin, setCommonPlugins } = require('./build.plugins');
 const { setupDotenvFilesForEnv } = require('./build.dotenv');
 
-const { _BUILD_RELATIVE_DIRNAME, DEV_BRANCH, DEV_PORT } = setupDotenvFilesForEnv({
-  env: 'development'
-});
+const { _BUILD_RELATIVE_DIRNAME, DEV_BRANCH, DEV_PORT } = setupDotenvFilesForEnv({ env: process.env.NODE_ENV });
 
 const { config: webpackConfig, plugins } = config({
   appUrl: ['/insights/subscriptions', '/openshift/subscriptions'],
