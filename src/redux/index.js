fee9474ada36f0353bf10bb8a7018ad4c70bfbb7
CD Cabrera <ccabrera@redhat.com>
2022-10-17 13:36:05 -0400

    DEV patch withRouter

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { store } from './store';
 import { reduxActions } from './actions';
 import { reduxHelpers, apiQueries } from './common';
@@ -7,6 +8,28 @@ import { storeHooks } from './hooks';
 import { reduxReducers } from './reducers';
 import { reduxSelectors } from './selectors';
 import { reduxTypes } from './types';
+
+/**
+ * Patch withRouter for legacy components.
+ *
+ * @param {React.ReactNode} Component
+ * @returns {function(*)}
+ */
+const withRouter = Component => props => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const params = useParams();
+  return (
+    <Component
+      {...props}
+      history={{ push: navigate }}
+      location={location}
+      navigate={navigate}
+      params={params}
+      router={{ location, navigate, params }}
+    />
+  );
+};
 
 /**
  * Wrapper for applying Router Dom withRouter and Redux connect.
