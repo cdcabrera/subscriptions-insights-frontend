d42bd64ac7dabf2a5d4ae5392f9643d276f101b9
CD Cabrera <ccabrera@redhat.com>
2022-11-17 02:38:36 -0500

    refactor(deprecated): sw-235 remove unused redux, components

--- src/redux/index.js
+++ src/redux/index.js
@@ -5,7 +5,6 @@ import { reduxActions } from './actions';
 import { reduxHelpers, apiQueries } from './common';
 import { storeHooks } from './hooks';
 import { reduxReducers } from './reducers';
-import { reduxSelectors } from './selectors';
 import { reduxTypes } from './types';
 
 /**
@@ -18,15 +17,4 @@ import { reduxTypes } from './types';
 const connectRouter = (mapStateToProps, mapDispatchToProps) => component =>
   withRouter(connect(mapStateToProps, mapDispatchToProps)(component));
 
-export {
-  apiQueries,
-  connect,
-  connectRouter,
-  reduxActions,
-  reduxHelpers,
-  reduxReducers,
-  reduxSelectors,
-  reduxTypes,
-  store,
-  storeHooks
-};
+export { apiQueries, connect, connectRouter, reduxActions, reduxHelpers, reduxReducers, reduxTypes, store, storeHooks };
