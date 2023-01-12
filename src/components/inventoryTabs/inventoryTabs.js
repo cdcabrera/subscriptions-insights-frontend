dfe328940392280eeb1dd3fe34043940f4ecad0d
CD Cabrera <ccabrera@redhat.com>
2023-01-12 12:37:40 -0500

    refactor(inventoryTabs): class to function, hooks

--- src/components/inventoryTabs/inventoryTabs.js
+++ src/components/inventoryTabs/inventoryTabs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Title } from '@patternfly/react-core';
-import { connect, reduxTypes, store } from '../../redux';
+import { reduxTypes, storeHooks } from '../../redux';
 import { Tabs } from '../tabs/tabs';
 import { helpers } from '../../common';
 import { translate } from '../i18n/i18n';
@@ -9,11 +9,34 @@ import { InventoryTab } from './inventoryTab';
 
 /**
  * A system inventory tabs component.
+ * Render inventory tabs using Inventory tab passed props only. A parallel outcome can be
+ * achieved by passing an array of objects through a prop.
  *
- * @augments React.Component
  * @fires onTab
+ * @param {object} props
+ * @param {number} props.activeTab
+ * @param {React.ReactNode} props.children
+ * @param {number} props.defaultActiveTab
+ * @param {boolean} props.isDisabled
+ * @param {string} props.productId
+ * @param {Function} props.t
+ * @param {Function} props.useDispatch
+ * @param {Function} props.useSelector
+ * @returns {React.ReactNode|null}
  */
-class InventoryTabs extends React.Component {
+const InventoryTabs = ({
+  activeTab,
+  children,
+  defaultActiveTab,
+  isDisabled,
+  productId,
+  t,
+  useDispatch: useAliasDispatch,
+  useSelector: useAliasSelector
+}) => {
+  const updatedActiveTab = useAliasSelector(({ inventory }) => inventory.tabs?.[productId], activeTab);
+  const dispatch = useAliasDispatch();
+
   /**
    * On tab update state.
    *
@@ -21,10 +44,8 @@ class InventoryTabs extends React.Component {
    * @param {object} params
    * @param {string} params.index tab index
    */
-  onTab = ({ index }) => {
-    const { productId } = this.props;
-
-    store.dispatch({
+  const onTab = ({ index }) => {
+    dispatch({
       type: reduxTypes.inventory.SET_INVENTORY_TAB,
       tabs: {
         [productId]: index
@@ -32,39 +53,29 @@ class InventoryTabs extends React.Component {
     });
   };
 
-  /**
-   * Render inventory tabs using Inventory tab passed props only. A parallel outcome can be
-   * achieved by passing an array of objects through a prop.
-   *
-   * @returns {Node}
-   */
-  render() {
-    const { activeTab, children, defaultActiveTab, isDisabled, t } = this.props;
-
-    if (isDisabled) {
-      return null;
-    }
-
-    const updatedChildren = React.Children.toArray(children).map((child, index) => {
-      const { props = {} } = child;
-
-      return {
-        active: props.active || false,
-        content: props.children || child,
-        title: props.title || t('curiosity-inventory.tabSubHeading', { count: index })
-      };
-    });
-
-    return (
-      <React.Fragment>
-        <Title headingLevel="h2" className="sr-only">
-          {t('curiosity-inventory.tabHeading', { count: updatedChildren.length })}
-        </Title>
-        <Tabs activeTab={activeTab} defaultActiveTab={defaultActiveTab} onTab={this.onTab} tabs={updatedChildren} />
-      </React.Fragment>
-    );
+  if (isDisabled) {
+    return null;
   }
-}
+
+  const updatedChildren = React.Children.toArray(children).map((child, index) => {
+    const { props: childProps = {} } = child;
+
+    return {
+      active: childProps.active || false,
+      content: childProps.children || child,
+      title: childProps.title || t('curiosity-inventory.tabSubHeading', { count: index })
+    };
+  });
+
+  return (
+    <React.Fragment>
+      <Title headingLevel="h2" className="sr-only">
+        {t('curiosity-inventory.tabHeading', { count: updatedChildren.length })}
+      </Title>
+      <Tabs activeTab={updatedActiveTab} defaultActiveTab={defaultActiveTab} onTab={onTab} tabs={updatedChildren} />
+    </React.Fragment>
+  );
+};
 
 /**
  * Prop types.
@@ -78,7 +89,9 @@ InventoryTabs.propTypes = {
   defaultActiveTab: PropTypes.number,
   isDisabled: PropTypes.bool,
   productId: PropTypes.string.isRequired,
-  t: PropTypes.func
+  t: PropTypes.func,
+  useDispatch: PropTypes.func,
+  useSelector: PropTypes.func
 };
 
 /**
@@ -90,16 +103,9 @@ InventoryTabs.defaultProps = {
   activeTab: 0,
   defaultActiveTab: 0,
   isDisabled: helpers.UI_DISABLED_TABLE,
-  t: translate
+  t: translate,
+  useDispatch: storeHooks.reactRedux.useDispatch,
+  useSelector: storeHooks.reactRedux.useSelector
 };
 
-/**
- * Create a selector from applied state, props.
- *
- * @type {Function}
- */
-const mapStateToProps = ({ inventory }, { productId }) => ({ activeTab: inventory.tabs?.[productId] });
-
-const ConnectedInventoryTabs = connect(mapStateToProps)(InventoryTabs);
-
-export { ConnectedInventoryTabs as default, ConnectedInventoryTabs, InventoryTabs, InventoryTab };
+export { InventoryTabs as default, InventoryTabs, InventoryTab };
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import { connect } from 'react-redux';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { store } from './store';
 import { reduxActions } from './actions';
 import { reduxHelpers } from './common';
@@ -8,36 +6,4 @@ import { storeHooks } from './hooks';
 import { reduxReducers } from './reducers';
 import { reduxTypes } from './types';
 
-/**
- * Patch withRouter for legacy components.
- *
- * @param {React.ReactNode} Component
- * @returns {function(*)}
- */
-const withRouter = Component => props => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const params = useParams();
-  return (
-    <Component
-      {...props}
-      history={{ push: navigate }}
-      location={location}
-      navigate={navigate}
-      params={params}
-      router={{ location, navigate, params }}
-    />
-  );
-};
-
-/**
- * Wrapper for applying Router Dom withRouter and Redux connect.
- *
- * @param {Function} mapStateToProps
- * @param {Function} mapDispatchToProps
- * @returns {Function}
- */
-const connectRouter = (mapStateToProps, mapDispatchToProps) => component =>
-  withRouter(connect(mapStateToProps, mapDispatchToProps)(component));
-
-export { connect, connectRouter, reduxActions, reduxHelpers, reduxReducers, reduxTypes, store, storeHooks };
+export { connect, reduxActions, reduxHelpers, reduxReducers, reduxTypes, store, storeHooks };
