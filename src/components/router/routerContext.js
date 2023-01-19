097e575f3b4e4bcb2d447e3fa2588d5a1f8d4104
CD Cabrera <ccabrera@redhat.com>
2023-01-19 15:59:41 -0500

    refactor(router): sw-625 move to router v6

--- src/components/router/routerContext.js
+++ src/components/router/routerContext.js
@@ -1,37 +1,159 @@
-import React, { useContext } from 'react';
-import { useHistory as useHistoryRRD, useLocation, useParams, useRouteMatch } from 'react-router-dom';
+import { useCallback } from 'react';
+import {
+  useLocation,
+  useNavigate as useRRDNavigate,
+  useParams,
+  useResolvedPath,
+  useSearchParams as useRRDSearchParams
+} from 'react-router-dom';
 import { routerHelpers } from './routerHelpers';
-import { helpers } from '../../common/helpers';
 
 /**
- * Route context.
+ * Return a callback for redirecting, and replacing, towards a new path, or url.
  *
- * @type {React.Context<{}>}
+ * @callback redirect
+ * @param {object} options
+ * @param {Function} options.useLocation
+ * @param {Function} options.useResolvedPath
+ * @returns {(function(*): void)|*}
  */
-const DEFAULT_CONTEXT = [
-  { routeDetail: { baseName: null, errorRoute: null, routes: [], routeItem: {} } },
-  helpers.noop
-];
+const useRedirect = ({
+  useLocation: useAliasLocation = useLocation,
+  useResolvedPath: useAliasResolvedPath = useResolvedPath
+} = {}) => {
+  const { pathname } = useAliasResolvedPath();
+  const { hash = '', search = '' } = useAliasLocation() || {};
 
-const RouterContext = React.createContext(DEFAULT_CONTEXT);
+  /**
+   * redirect
+   *
+   * @param {string} route
+   * @returns {undefined}
+   */
+  return useCallback(
+    route => {
+      const baseName = routerHelpers.dynamicBaseName({ pathName: pathname });
+      let isUrl;
 
-/**
- * Get an updated router context.
- *
- * @returns {React.Context<{}>}
- */
-const useRouterContext = () => useContext(RouterContext);
+      try {
+        isUrl = !!new URL(route);
+      } catch (e) {
+        isUrl = false;
+      }
+
+      window.location.replace((isUrl && route) || `${routerHelpers.pathJoin(baseName, route)}${search}${hash}`);
+      return undefined;
+    },
+    [hash, pathname, search]
+  );
+};
 
 /**
  * Get a route detail from router context.
  *
  * @param {object} options
- * @param {Function} options.useRouterContext
- * @returns {{routes: Array, routeItem: object, baseName: string, errorRoute: object}}
+ * @param {Function} options.useRedirect
+ * @param {Function} options.useParams
+ * @returns {{baseName: string, errorRoute: object}}
  */
-const useRouteDetail = ({ useRouterContext: useAliasRouterContext = useRouterContext } = {}) => {
-  const { routeDetail } = useAliasRouterContext();
-  return routeDetail;
+const useRouteDetail = ({
+  useRedirect: useAliasRedirect = useRedirect,
+  useParams: useAliasParams = useParams
+} = {}) => {
+  const redirect = useAliasRedirect();
+  const { productPath } = useAliasParams();
+  const { allConfigs, configs, firstMatch } = routerHelpers.getRouteConfigByPath({ pathName: productPath });
+
+  if (!firstMatch) {
+    redirect(routerHelpers.redirectRoute.redirect);
+  }
+
+  return {
+    allProductConfigs: allConfigs,
+    firstMatch,
+    errorRoute: routerHelpers.errorRoute,
+    productGroup: firstMatch?.productGroup,
+    productConfig: (configs?.length && configs) || []
+  };
+};
+
+// ToDo: align useNavigate with updates from the useHistory proxy
+/**
+ * useNavigate wrapper, apply application config context routing
+ *
+ * @param {object} options
+ * @param {Function} options.useLocation
+ * @param {Function} options.useNavigate
+ * @param {Function} options.useResolvedPath
+ * @returns {Function}
+ */
+const useNavigate = ({
+  useLocation: useAliasLocation = useLocation,
+  useNavigate: useAliasNavigate = useRRDNavigate,
+  useResolvedPath: useAliasResolvedPath = useResolvedPath
+} = {}) => {
+  const { search, hash } = useAliasLocation();
+  const navigate = useAliasNavigate();
+  const { pathname } = useAliasResolvedPath();
+
+  return useCallback(
+    (path, { isLeftNav = false, isPassSearchHash = true, ...options } = {}) => {
+      if (isLeftNav) {
+        return undefined;
+      }
+
+      const { firstMatch } = routerHelpers.getRouteConfigByPath({ pathName: path });
+
+      if (firstMatch) {
+        const dynamicBaseName = routerHelpers.dynamicBaseName({ pathName: pathname });
+        const updatedPath = `${dynamicBaseName}/${firstMatch.productPath}`;
+
+        return navigate((isPassSearchHash && `${updatedPath}${search}${hash}`) || updatedPath, options);
+      }
+
+      return navigate((isPassSearchHash && `${path}${search}${hash}`) || path, options);
+    },
+    [hash, navigate, pathname, search]
+  );
+};
+
+/**
+ * Search parameter, return
+ *
+ * @param {object} options
+ * @param {Function} options.useLocation
+ * @param {Function} options.useSearchParams
+ * @returns {Array}
+ */
+const useSearchParams = ({
+  useSearchParams: useAliasSearchParams = useRRDSearchParams,
+  useLocation: useAliasLocation = useLocation
+} = {}) => {
+  const { search } = useAliasLocation();
+  const [, setAliasSearchParams] = useAliasSearchParams();
+
+  /**
+   * Alias returned React Router Dom useSearchParams hook to something expected.
+   * Defaults to merging search objects instead of overwriting them.
+   *
+   * @param {object} updatedQuery
+   * @param {object} options
+   * @param {boolean} options.isMerged Merge search with existing search, or don't
+   * @param {string|*} options.currentSearch search returned from useLocation
+   */
+  const setSearchParams = (updatedQuery, { isMerged = true, currentSearch = search } = {}) => {
+    let updatedSearch = {};
+
+    if (isMerged) {
+      Object.assign(updatedSearch, routerHelpers.parseSearchParams(currentSearch), updatedQuery);
+    } else {
+      updatedSearch = updatedQuery;
+    }
+
+    setAliasSearchParams(updatedSearch);
+  };
+
+  return [routerHelpers.parseSearchParams(search), setSearchParams];
 };
 
 /**
@@ -41,6 +163,7 @@ const useRouteDetail = ({ useRouterContext: useAliasRouterContext = useRouterCon
  * @param {Function} options.useHistory
  * @returns {object}
  */
+/*
 const useHistory = ({ useHistory: useAliasHistory = useHistoryRRD } = {}) => {
   const history = useAliasHistory();
 
@@ -60,27 +183,24 @@ const useHistory = ({ useHistory: useAliasHistory = useHistoryRRD } = {}) => {
     }
   };
 };
+ */
 
 const context = {
-  RouterContext,
-  DEFAULT_CONTEXT,
-  useHistory,
   useLocation,
+  useNavigate,
   useParams,
+  useRedirect,
   useRouteDetail,
-  useRouteMatch,
-  useRouterContext
+  useSearchParams
 };
 
 export {
   context as default,
   context,
-  RouterContext,
-  DEFAULT_CONTEXT,
-  useHistory,
   useLocation,
+  useNavigate,
   useParams,
+  useRedirect,
   useRouteDetail,
-  useRouteMatch,
-  useRouterContext
+  useSearchParams
 };
