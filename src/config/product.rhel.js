4fc1df62f0eb9bf8558f526cf5b7801e2803128b
CD Cabrera <ccabrera@redhat.com>
2023-01-24 15:52:54 -0500

    DEV insights alias for RHEL, restore

--- src/config/product.rhel.js
+++ src/config/product.rhel.js
@@ -54,7 +54,7 @@ const productLabel = RHSM_API_PATH_PRODUCT_TYPES.RHEL;
  *     initialSubscriptionsInventoryFilters: {}[], initialToolbarFilters: {}[], }}
  */
 const config = {
-  aliases: [],
+  aliases: ['insights'],
   // pathParameter: productId,
   // productParameter: productGroup,
   productGroup,
