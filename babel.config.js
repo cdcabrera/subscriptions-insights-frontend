10737d004e0f8be7cd23ef6127f5025764938975
CD Cabrera <cabrera.code@gmail.com>
2023-06-28 23:56:31 -0400

    build(dotenv,testing): sw-1348 update babel presets
    * dotenv, fix setting params
    * build, babel testing presets, browser targets

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,6 @@ const { setupDotenvFilesForEnv } = require('./config/build.dotenv');
 setupDotenvFilesForEnv({ env: process.env.NODE_ENV || 'production' });
 
 module.exports = {
-  presets: ['react-app']
+  targets: '> 0.25%, not dead',
+  presets: ['@babel/preset-env', '@babel/preset-react']
 };
