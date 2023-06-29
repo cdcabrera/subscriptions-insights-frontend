0a0475cb3a470fd849f551555edeb14278223cd6
CD Cabrera <cabrera.code@gmail.com>
2023-06-28 23:56:31 -0400

    build(testing): sw-1348 babel presets

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,30 @@ const { setupDotenvFilesForEnv } = require('./config/build.dotenv');
 setupDotenvFilesForEnv({ env: process.env.NODE_ENV || 'production' });
 
 module.exports = {
-  presets: ['react-app']
+  targets: '> 0.25%, not dead',
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    // '@babel/plugin-transform-async-generator-functions'
+    /*
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: false,
+        regenerator: true,
+        useEsModules: false
+      }
+    ]
+    */
+  ]
+  /*
+  plugins: [
+    [
+      '@babel/plugin-transform-arrow-functions',
+      {
+        spec: true
+      }
+    ]
+  ]
+  */
 };
