59b3e951d24d70b7656f98a5569133e4c28ec8d4
CD Cabrera <cabrera.code@gmail.com>
2023-06-29 01:02:01 -0400

    DEV stash

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,29 @@ setupDotenvFilesForEnv({ env: process.env.NODE_ENV || 'production' });
 
 module.exports = {
   targets: '> 0.25%, not dead',
-  presets: ['@babel/preset-env', '@babel/preset-react']
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-transform-runtime'
+    /*
+    [
+      'transform-imports',
+      {
+        '@patternfly/react-icons': {
+          transform: importName => {
+            if (importName === 'IconSize') {
+              return `@patternfly/react-icons/dist/js/createIcon`;
+            }
+
+            return `@patternfly/react-icons/dist/js/icons/${importName
+              .split(/(?=[A-Z])/)
+              .join('-')
+              .toLowerCase()}`;
+          },
+          preventFullImport: true
+        }
+      },
+      'react-icons'
+    ]
+     */
+  ]
 };
