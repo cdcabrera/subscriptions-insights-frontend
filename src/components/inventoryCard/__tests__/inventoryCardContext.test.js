bfaf696b3021200d918213906b4f3a6f349e12b6
CD Cabrera <cabrera.code@gmail.com>
2023-07-21 13:45:53 -0400

    refactor(rhsmServices): sw-870 remove, replace hosts refs

--- src/components/inventoryCard/__tests__/inventoryCardContext.test.js
+++ src/components/inventoryCard/__tests__/inventoryCardContext.test.js
@@ -1,9 +1,7 @@
 import {
   context,
-  useGetHostsInventory,
   useGetInstancesInventory,
   useOnPageInstances,
-  useOnColumnSortHosts,
   useOnColumnSortInstances
 } from '../inventoryCardContext';
 import {
@@ -18,29 +16,6 @@ describe('InventoryCardContext', () => {
 
   it('should expect specific sort properties', () => {
     expect(SORT_TYPES).toMatchSnapshot('sort properties');
-  });
-
-  it('should handle a store response with useGetHostsInventory', async () => {
-    const { result } = await shallowHook(
-      () =>
-        useGetHostsInventory({
-          useProduct: () => ({ productId: 'lorem' })
-        }),
-      {
-        state: {
-          inventory: {
-            hostsInventory: {
-              lorem: {
-                fulfilled: true,
-                data: [{ data: [{ lorem: 'ipsum' }, { dolor: 'sit' }], meta: {} }]
-              }
-            }
-          }
-        }
-      }
-    );
-
-    expect(result).toMatchSnapshot('store response');
   });
 
   it('should handle a store response with useGetInstancesInventory', async () => {
@@ -64,70 +39,6 @@ describe('InventoryCardContext', () => {
     );
 
     expect(result).toMatchSnapshot('store response');
-  });
-
-  it('should handle variations in hosts inventory API responses', async () => {
-    const { result: errorResponse } = shallowHook(() =>
-      useGetHostsInventory({
-        useProduct: () => ({ productId: 'lorem' }),
-        useDispatch: () => {},
-        useProductInventoryQuery: () => ({}),
-        useSelectorsResponse: () => ({ error: true })
-      })
-    );
-
-    expect(errorResponse).toMatchSnapshot('inventory, error');
-
-    const { result: pendingResponse } = shallowHook(() =>
-      useGetHostsInventory({
-        useProduct: () => ({ productId: 'lorem' }),
-        useDispatch: () => {},
-        useProductInventoryQuery: () => ({}),
-        useSelectorsResponse: () => ({ pending: true })
-      })
-    );
-
-    expect(pendingResponse).toMatchSnapshot('inventory, pending');
-
-    const { result: cancelledResponse } = shallowHook(() =>
-      useGetHostsInventory({
-        useProduct: () => ({ productId: 'lorem' }),
-        useDispatch: () => {},
-        useProductInventoryQuery: () => ({}),
-        useSelectorsResponse: () => ({ cancelled: true })
-      })
-    );
-
-    expect(cancelledResponse).toMatchSnapshot('inventory, cancelled');
-
-    const mockFulfilledGetInventory = jest.fn();
-    const { result: fulfilledResponse } = await mountHook(() =>
-      useGetHostsInventory({
-        getInventory: () => mockFulfilledGetInventory,
-        useProduct: () => ({ productId: 'lorem' }),
-        useDispatch: () => {},
-        useProductInventoryQuery: () => ({}),
-        useSelectorsResponse: () => ({ fulfilled: true })
-      })
-    );
-
-    expect(mockFulfilledGetInventory).toHaveBeenCalledTimes(1);
-    expect(fulfilledResponse).toMatchSnapshot('inventory, fulfilled');
-
-    const mockDisabledGetInventory = jest.fn();
-    const { result: disabledResponse } = await mountHook(() =>
-      useGetHostsInventory({
-        isDisabled: true,
-        getInventory: () => mockDisabledGetInventory,
-        useProduct: () => ({ productId: 'lorem' }),
-        useDispatch: () => {},
-        useProductInventoryQuery: () => ({}),
-        useSelectorsResponse: () => ({})
-      })
-    );
-
-    expect(mockDisabledGetInventory).toHaveBeenCalledTimes(0);
-    expect(disabledResponse).toMatchSnapshot('inventory, disabled');
   });
 
   it('should handle variations in instances inventory API responses', async () => {
@@ -203,21 +114,6 @@ describe('InventoryCardContext', () => {
 
     onPage({ offset: 1, perPage: 5 });
     expect(mockDispatch.mock.calls).toMatchSnapshot('onPage event, dispatch');
-    mockDispatch.mockClear();
-  });
-
-  it('should handle an onColumnSort event for hosts', () => {
-    const mockDispatch = jest.fn();
-    const onColumnSort = useOnColumnSortHosts({
-      sortColumns: { LOREM_IPSUM_COLUMN_ONE: 'loremIpsumColumnOne' },
-      useDispatch: () => mockDispatch,
-      useProduct: () => ({ productId: 'lorem' })
-    });
-
-    onColumnSort(null, { direction: SORT_DIRECTION_TYPES.DESCENDING, id: 'loremIpsumColumnOne' });
-    onColumnSort(null, { direction: SORT_DIRECTION_TYPES.ASCENDING, id: 'loremIpsumColumnOne' });
-
-    expect(mockDispatch.mock.calls).toMatchSnapshot('onColumnSort event, dispatch hosts');
     mockDispatch.mockClear();
   });
 
