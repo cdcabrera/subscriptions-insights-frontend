9e268267f6273d1637a81225de1352807ac4bdb0
CD Cabrera <cabrera.code@gmail.com>
2023-07-28 12:01:17 -0400

    refactor(table): sw-1537 migrate to pf composable table

---
+++ src/components/inventoryCard/_inventoryCardContext.js
@@ -0,0 +1,426 @@
+import React, { useMemo } from 'react';
+import { useMount, useShallowCompareEffect } from 'react-use';
+import { ToolbarItem } from '@patternfly/react-core';
+import { reduxActions, reduxTypes, storeHooks } from '../../redux';
+import { useSession } from '../authentication/authenticationContext';
+import {
+  useProduct,
+  useProductInventoryHostsConfig,
+  useProductInventoryHostsQuery
+} from '../productView/productViewContext';
+import {
+  RHSM_API_QUERY_INVENTORY_SORT_DIRECTION_TYPES as SORT_DIRECTION_TYPES,
+  RHSM_API_QUERY_INVENTORY_SORT_TYPES as SORT_TYPES,
+  RHSM_API_QUERY_SET_TYPES
+} from '../../services/rhsm/rhsmConstants';
+import { helpers } from '../../common';
+import { inventoryCardHelpers } from './_inventoryCardHelpers';
+import { tableHelpers } from '../table/_table';
+import { toolbarFieldOptions } from '../toolbar/toolbarFieldSelectCategory';
+
+/**
+ * @memberof InventoryCard
+ * @module InventoryCardContext
+ */
+
+/**
+ * Parse filters settings for context.
+ *
+ * @param {object} options
+ * @param {boolean} options.isDisabled
+ * @param {Function} options.useProduct
+ * @param {Function} options.useProductConfig
+ * @returns {{standaloneFiltersSettings: Array<{ settings: object }>, groupedFiltersSettings: { settings: object }}}
+ */
+const useParseInstancesFiltersSettings = ({
+  isDisabled = false,
+  useProduct: useAliasProduct = useProduct,
+  useProductConfig: useAliasProductConfig = useProductInventoryHostsConfig
+} = {}) => {
+  const { productId } = useAliasProduct();
+  const { filters = [], settings = {} } = useAliasProductConfig();
+
+  return useMemo(() => {
+    if (isDisabled) {
+      return undefined;
+    }
+    return inventoryCardHelpers.normalizeInventorySettings({
+      filters,
+      settings,
+      productId
+    });
+  }, [filters, isDisabled, settings, productId]);
+};
+
+const useSelectorInstances = ({
+  useParseInstancesFiltersSettings: useAliasParseInstancesFiltersSettings = useParseInstancesFiltersSettings,
+  useProduct: useAliasProduct = useProduct,
+  useProductInventoryQuery: useAliasProductInventoryQuery = useProductInventoryHostsQuery,
+  useSelectorsResponse: useAliasSelectorsResponse = storeHooks.reactRedux.useSelectorsResponse,
+  useSession: useAliasSession = useSession
+} = {}) => {
+  const { productId } = useAliasProduct();
+  const session = useAliasSession();
+  const query = useAliasProductInventoryQuery();
+  const { columnCountAndWidths, filters, settings } = useAliasParseInstancesFiltersSettings();
+  const response = useAliasSelectorsResponse(({ inventory }) => inventory?.instancesInventory?.[productId]);
+
+  const { pending, cancelled, data, ...restResponse } = response;
+  const updatedPending = pending || cancelled || false;
+  let parsedData;
+
+  if (response?.fulfilled) {
+    const updatedData = (data?.length === 1 && data[0]) || data || {};
+    parsedData = inventoryCardHelpers.parseInventoryResponse({
+      data: updatedData,
+      filters,
+      query,
+      session,
+      settings
+    });
+  }
+
+  return {
+    ...restResponse,
+    pending: updatedPending,
+    resultsColumnCountAndWidths: columnCountAndWidths,
+    ...parsedData
+  };
+
+  /*
+  return useMemo(() => {
+    const { pending, cancelled, data, ...restResponse } = response;
+    const updatedPending = pending || cancelled || false;
+    let parsedData;
+
+    if (response?.fulfilled) {
+      const updatedData = (data?.length === 1 && data[0]) || data || {};
+      parsedData = inventoryCardHelpers.parseInventoryResponse({
+        data: updatedData,
+        filters,
+        query,
+        session,
+        settings
+      });
+    }
+
+    return {
+      ...restResponse,
+      pending: updatedPending,
+      resultsColumnCountAndWidths: columnCountAndWidths,
+      ...parsedData
+    };
+  }, [columnCountAndWidths, filters, query, response, session, settings]);
+  */
+};
+
+/**
+ * Combined Redux RHSM Actions, getInstancesInventory, and inventory selector response.
+ *
+ * @param {object} options
+ * @param {boolean} options.isDisabled
+ * @param {Function} options.getInventory
+ * @param {Function} options.useDispatch
+ * @param {Function} options.useProduct
+ * @param {Function} options.useProductInventoryQuery
+ * @param {Function} options.useSelector
+ * @returns {{data: (*|{}|Array|{}), pending: boolean, fulfilled: boolean, error: boolean}}
+ */
+const useGetInstancesInventory = ({
+  isDisabled = false,
+  getInventory = reduxActions.rhsm.getInstancesInventory,
+  useDispatch: useAliasDispatch = storeHooks.reactRedux.useDispatch,
+  useSelector: useAliasSelector = useSelectorInstances,
+  // useParseInstancesFiltersSettings: useAliasParseInstancesFiltersSettings = useParseInstancesFiltersSettings,
+  useProduct: useAliasProduct = useProduct,
+  useProductInventoryQuery: useAliasProductInventoryQuery = useProductInventoryHostsQuery
+  // useSelectorsResponse: useAliasSelectorsResponse = storeHooks.reactRedux.useSelectorsResponse,
+  // useSession: useAliasSession = useSession
+} = {}) => {
+  const { productId } = useAliasProduct();
+  // const session = useAliasSession();
+  const query = useAliasProductInventoryQuery();
+  const dispatch = useAliasDispatch();
+  // const { columnCountAndWidths, filters, settings } = useAliasParseInstancesFiltersSettings();
+  // const response = useAliasSelectorsResponse(({ inventory }) => inventory?.instancesInventory?.[productId]);
+  const response = useAliasSelector();
+
+  /*
+  const { productId } = {}; // useAliasProduct();
+  const session = {}; // useAliasSession();
+  const query = {}; // useAliasProductInventoryQuery();
+  const dispatch = Function.prototype; // useAliasDispatch();
+  const { columnCountAndWidths, filters, settings } = {}; // useAliasParseInstancesFiltersSettings();
+  const { cancelled, pending, data, ...response } = {}; // useAliasSelectorsResponse(
+  //  ({ inventory }) => inventory?.instancesInventory?.[productId]
+  // );
+  const updatedPending = pending || cancelled || false;
+  */
+
+  useMount(() => {
+    console.log(
+      '>>>> MOUNTED INVENTORY'
+      // useAliasDispatch,
+      // useAliasProduct,
+      // useAliasSession,
+      // useAliasSelectorsResponse,
+      // useAliasParseInstancesFiltersSettings,
+      // useAliasProductInventoryQuery
+    );
+  });
+
+  useShallowCompareEffect(() => {
+    if (!isDisabled) {
+      getInventory(productId, query)(dispatch);
+    }
+  }, [isDisabled, productId, query]);
+
+  return response;
+
+  /*
+  return useMemo(() => {
+    const { pending, cancelled, data, ...restResponse } = response;
+    const updatedPending = pending || cancelled || false;
+    let parsedData;
+
+    if (response?.fulfilled) {
+      const updatedData = (data?.length === 1 && data[0]) || data || {};
+      parsedData = inventoryCardHelpers.parseInventoryResponse({
+        data: updatedData,
+        filters,
+        query,
+        session,
+        settings
+      });
+    }
+
+    return {
+      ...restResponse,
+      pending: updatedPending,
+      resultsColumnCountAndWidths: columnCountAndWidths,
+      ...parsedData
+    };
+  }, [columnCountAndWidths, filters, query, response, session, settings]);
+   */
+  /*
+  const parsedData = useMemo(() => {
+    if (response?.fulfilled) {
+      const updatedData = (data?.length === 1 && data[0]) || data || {};
+      return inventoryCardHelpers.parseInventoryResponse({ data: updatedData, filters, query, session, settings });
+    }
+
+    return undefined;
+  }, [data, filters, query, response?.fulfilled, session, settings]);
+
+  return {
+    ...response,
+    pending: updatedPending,
+    resultsColumnCountAndWidths: columnCountAndWidths,
+    ...parsedData
+  };
+  */
+};
+
+/**
+ * Return a component list for a configurable inventoryCard action toolbar.
+ * Allow the "content" prop to receive inventory data for display via callback.
+ *
+ * @param {object} options
+ * @param {Array} options.categoryOptions
+ * @param {Function} options.useSelector
+ * @param {Function} options.useProductConfig
+ * @returns {Array}
+ */
+const useInventoryCardActionsInstances = ({
+  categoryOptions = toolbarFieldOptions,
+  // useGetInventory: useAliasGetInventory = useGetInstancesInventory,
+  useSelector: useAliasSelector = useSelectorInstances,
+  useProductConfig: useAliasProductConfig = useProductInventoryHostsConfig
+} = {}) => {
+  const results = useAliasSelector();
+  const { pending, resultsCount } = results;
+  const { settings = {} } = useAliasProductConfig();
+  const { actions } = settings;
+
+  return useMemo(
+    () =>
+      actions?.map(({ id, content, ...actionProps }) => {
+        const option = categoryOptions.find(({ value: categoryOptionValue }) => id === categoryOptionValue);
+        const { component: OptionComponent } = option || {};
+
+        return (
+          (OptionComponent && (
+            <ToolbarItem key={`option-${id}`}>
+              <OptionComponent isFilter={false} {...actionProps} />
+            </ToolbarItem>
+          )) ||
+          (content && !pending && resultsCount && (
+            <ToolbarItem key={id || helpers.generateId()}>
+              {typeof content === 'function' ? content({ data: results }) : content}
+            </ToolbarItem>
+          )) ||
+          null
+        );
+      }),
+    [actions, categoryOptions, results, resultsCount, pending]
+  );
+};
+/* works
+const useInventoryCardActionsInstances = (
+  { results, settings },
+  {
+    categoryOptions = toolbarFieldOptions
+    // useGetInventory: useAliasGetInventory = useGetInstancesInventory,
+    // useProductConfig: useAliasProductConfig = useProductInventoryHostsConfig
+  } = {}
+) => {
+  // const results = useAliasGetInventory();
+  const { pending, resultsCount } = results;
+  // const { settings = {} } = useAliasProductConfig();
+  const { actions } = settings;
+
+  return useMemo(
+    () =>
+      actions?.map(({ id, content, ...actionProps }) => {
+        const option = categoryOptions.find(({ value: categoryOptionValue }) => id === categoryOptionValue);
+        const { component: OptionComponent } = option || {};
+
+        return (
+          (OptionComponent && (
+            <ToolbarItem key={`option-${id}`}>
+              <OptionComponent isFilter={false} {...actionProps} />
+            </ToolbarItem>
+          )) ||
+          (content && !pending && resultsCount && (
+            <ToolbarItem key={id || helpers.generateId()}>
+              {typeof content === 'function' ? content({ data: results }) : content}
+            </ToolbarItem>
+          )) ||
+          null
+        );
+      }),
+    [actions, categoryOptions, results, resultsCount, pending]
+  );
+};
+
+ */
+
+/**
+ * An onPage callback for instances inventory.
+ *
+ * @param {object} options
+ * @param {Function} options.useDispatch
+ * @param {Function} options.useProduct
+ * @returns {Function}
+ */
+const useOnPageInstances = ({
+  useDispatch: useAliasDispatch = storeHooks.reactRedux.useDispatch,
+  useProduct: useAliasProduct = useProduct
+} = {}) => {
+  const { productId } = useAliasProduct();
+  const dispatch = useAliasDispatch();
+
+  /**
+   * On event update state for instances inventory.
+   *
+   * @event onPage
+   * @param {object} params
+   * @param {number} params.offset
+   * @param {number} params.perPage
+   * @returns {void}
+   */
+  return ({ offset, perPage }) => {
+    dispatch([
+      {
+        type: reduxTypes.query.SET_QUERY_RHSM_HOSTS_INVENTORY_TYPES[RHSM_API_QUERY_SET_TYPES.OFFSET],
+        viewId: productId,
+        [RHSM_API_QUERY_SET_TYPES.OFFSET]: offset
+      },
+      {
+        type: reduxTypes.query.SET_QUERY_RHSM_HOSTS_INVENTORY_TYPES[RHSM_API_QUERY_SET_TYPES.LIMIT],
+        viewId: productId,
+        [RHSM_API_QUERY_SET_TYPES.LIMIT]: perPage
+      }
+    ]);
+  };
+};
+
+/**
+ * An onColumnSort callback for instances inventory.
+ *
+ * @param {object} options
+ * @param {object} options.sortColumns
+ * @param {Function} options.useDispatch
+ * @param {Function} options.useProduct
+ * @returns {Function}
+ */
+const useOnColumnSortInstances = ({
+  sortColumns = SORT_TYPES,
+  useDispatch: useAliasDispatch = storeHooks.reactRedux.useDispatch,
+  useProduct: useAliasProduct = useProduct
+} = {}) => {
+  const { productId } = useAliasProduct();
+  const dispatch = useAliasDispatch();
+
+  /**
+   * On event update state for instances inventory.
+   *
+   * @event onColumnSort
+   * @param {object} params
+   * @param {string} params.direction
+   * @param {object} params.data
+   * @returns {void}
+   */
+  return ({ direction, data = {} }) => {
+    const { metric: id } = data;
+    const updatedSortColumn = Object.values(sortColumns).find(value => value === id);
+    let updatedDirection;
+
+    if (!updatedSortColumn) {
+      if (helpers.DEV_MODE || helpers.REVIEW_MODE) {
+        console.warn(`Sorting can only be performed on select fields, confirm field ${id} is allowed.`);
+      }
+      return;
+    }
+
+    switch (direction) {
+      case tableHelpers.SortByDirectionVariant.desc:
+        updatedDirection = SORT_DIRECTION_TYPES.DESCENDING;
+        break;
+      default:
+        updatedDirection = SORT_DIRECTION_TYPES.ASCENDING;
+        break;
+    }
+
+    dispatch([
+      {
+        type: reduxTypes.query.SET_QUERY_RHSM_HOSTS_INVENTORY_TYPES[RHSM_API_QUERY_SET_TYPES.DIRECTION],
+        viewId: productId,
+        [RHSM_API_QUERY_SET_TYPES.DIRECTION]: updatedDirection
+      },
+      {
+        type: reduxTypes.query.SET_QUERY_RHSM_HOSTS_INVENTORY_TYPES[RHSM_API_QUERY_SET_TYPES.SORT],
+        viewId: productId,
+        [RHSM_API_QUERY_SET_TYPES.SORT]: updatedSortColumn
+      }
+    ]);
+  };
+};
+
+const context = {
+  useGetInstancesInventory,
+  useInventoryCardActionsInstances,
+  useOnPageInstances,
+  useOnColumnSortInstances,
+  useParseInstancesFiltersSettings
+};
+
+export {
+  context as default,
+  context,
+  useGetInstancesInventory,
+  useInventoryCardActionsInstances,
+  useOnPageInstances,
+  useOnColumnSortInstances,
+  useParseInstancesFiltersSettings
+};
---
+++ src/components/inventoryCard/_inventoryCardHelpers.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { translate } from '../i18n/i18n';
+import { RHSM_API_QUERY_SET_TYPES, RHSM_API_RESPONSE_META_TYPES } from '../../services/rhsm/rhsmConstants';
+import { InventoryGuests } from '../inventoryGuests/_inventoryGuests'; // eslint-disable-line
+import { tableHelpers } from '../table/_table';
+
+/**
+ * @memberof InventoryCard
+ * @module InventoryCardHelpers
+ */
+
+/**
+ * Normalize inventory filters, settings into a consistent format.
+ *
+ * @param {object} params
+ * @param {Array} params.filters
+ * @param {object} params.settings
+ * @param {string} params.productId
+ * @returns {{settings: {}, columnCountAndWidths: {count: number, widths: []}, filters: []}}
+ */
+const normalizeInventorySettings = ({ filters = [], settings = {}, productId } = {}) => {
+  const updatedFilters = [];
+  const columnCountAndWidths = { count: filters.length, widths: [] };
+
+  filters.forEach(({ metric, header, cell, width, ...rest }) => {
+    let updatedHeader;
+    let updatedCell;
+    let updatedWidth;
+
+    if (typeof header === 'function' && header) {
+      updatedHeader = header;
+    } else if (header) {
+      updatedHeader = () => header;
+    } else {
+      updatedHeader = () => translate('curiosity-inventory.header', { context: [metric, productId] });
+    }
+
+    if (typeof cell === 'function' && cell) {
+      updatedCell = cell;
+    } else if (cell) {
+      updatedCell = () => cell;
+    } else {
+      updatedCell = ({ [metric]: displayValue }) => displayValue;
+    }
+
+    if (typeof width === 'number' && !Number.isNaN(width)) {
+      updatedWidth = width;
+    }
+
+    columnCountAndWidths.widths.push(updatedWidth);
+
+    updatedFilters.push({
+      label: translate('curiosity-inventory.header', { context: [metric, productId] }),
+      metric,
+      width,
+      ...rest,
+      header: updatedHeader,
+      cell: updatedCell
+    });
+  });
+
+  return {
+    columnCountAndWidths,
+    filters: updatedFilters,
+    settings
+  };
+};
+
+// ToDo: evaluate moving isWrap under the table component
+// ToDo: evaluate a fallback "perPageDefault = 10" defined here
+/**
+ * Parse an inventory API response against available filters, query parameters, and session values.
+ *
+ * @param {object} params
+ * @param {object} params.data
+ * @param {Array} params.filters
+ * @param {object} params.query
+ * @param {object} params.session
+ * @param {object} params.settings
+ * @returns {{dataSetColumnHeaders: [], resultsPerPage: number, resultsOffset: number, dataSetRows: [], resultsCount: number}}
+ */
+const parseInventoryResponse = ({ data = {}, filters = [], query = {}, session = {}, settings = {} } = {}) => {
+  const { data: listData = [], meta = {} } = data;
+  const resultsCount = meta[RHSM_API_RESPONSE_META_TYPES.COUNT];
+  const {
+    [RHSM_API_QUERY_SET_TYPES.OFFSET]: resultsOffset,
+    [RHSM_API_QUERY_SET_TYPES.LIMIT]: resultsPerPage,
+    [RHSM_API_QUERY_SET_TYPES.SORT]: sortColumn,
+    [RHSM_API_QUERY_SET_TYPES.DIRECTION]: sortDirection
+  } = query;
+
+  const dataSetColumnHeaders = [];
+  const dataSetRows = [];
+  const columnData = {};
+
+  listData.forEach(rowData => {
+    const dataSetRow = [];
+    let expandedContent;
+
+    filters.forEach(({ metric, label, cell, ...rest }) => {
+      const updatedCell = cell({ ...rowData }, { ...session }, { ...meta });
+      dataSetRow.push({ metric, ...rest, dataLabel: label, content: updatedCell });
+
+      columnData[metric] ??= [];
+      columnData[metric].push(updatedCell);
+    });
+
+    if (typeof settings?.guestContent === 'function') {
+      const guestContentResults = settings.guestContent({ ...rowData }, { ...session }, { ...meta });
+      const { id: guestId, numberOfGuests } = guestContentResults || {};
+
+      if (guestId && numberOfGuests) {
+        expandedContent = () => (
+          <InventoryGuests key={`guests-${guestId}`} id={guestId} numberOfGuests={numberOfGuests} />
+        );
+      }
+    }
+
+    dataSetRows.push({ cells: dataSetRow, row: rowData, expandedContent });
+  });
+
+  filters.forEach(({ metric, header, ...rest }) => {
+    const updatedHeader = header({ ...columnData[metric] }, { ...session }, { ...meta });
+    const updatedRest = { ...rest };
+
+    if (updatedRest.isSort === true && sortDirection && sortColumn === metric) {
+      updatedRest.isSortActive = true;
+      updatedRest.sortDirection = sortDirection;
+    }
+
+    if (updatedRest.isWrap === true) {
+      updatedRest.modifier = tableHelpers.WrapModifierVariant.wrap;
+    }
+
+    dataSetColumnHeaders.push({ metric, ...updatedRest, content: updatedHeader });
+  });
+
+  return {
+    dataSetColumnHeaders,
+    dataSetRows,
+    resultsCount,
+    resultsOffset,
+    resultsPerPage
+  };
+};
+
+const inventoryCardHelpers = {
+  normalizeInventorySettings,
+  parseInventoryResponse
+};
+
+export { inventoryCardHelpers as default, inventoryCardHelpers, normalizeInventorySettings, parseInventoryResponse };
