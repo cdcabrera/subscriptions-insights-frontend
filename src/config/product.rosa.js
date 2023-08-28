55bac384e65d45aa64ee8d54dbf7d795d85db9fb
CD Cabrera <cabrera.code@gmail.com>
2023-08-28 14:47:40 -0400

    DEV config openshift header strings, filter updates

--- src/config/product.rosa.js
+++ src/config/product.rosa.js
@@ -177,7 +177,7 @@ const config = {
   },
   initialGuestsFilters: [
     {
-      id: INVENTORY_TYPES.DISPLAY_NAME,
+      metric: INVENTORY_TYPES.DISPLAY_NAME,
       header: () => translate('curiosity-inventory.header', { context: ['guests', INVENTORY_TYPES.DISPLAY_NAME] }),
       cell: ({
         [INVENTORY_TYPES.DISPLAY_NAME]: displayName = {},
@@ -186,11 +186,11 @@ const config = {
         // FixMe: Disabled, see SWATCH-1209 for resolution
         const { inventory: authorized = false } = {};
 
-        if (!inventoryId?.value) {
-          return displayName?.value;
+        if (!inventoryId) {
+          return displayName;
         }
 
-        let updatedDisplayName = displayName.value || inventoryId.value;
+        let updatedDisplayName = displayName || inventoryId;
 
         if (authorized) {
           updatedDisplayName = (
@@ -198,7 +198,7 @@ const config = {
               isInline
               component="a"
               variant="link"
-              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/insights/inventory/${inventoryId.value}/`}
+              href={`${helpers.UI_DEPLOY_PATH_LINK_PREFIX}/insights/inventory/${inventoryId}/`}
             >
               {updatedDisplayName}
             </Button>
@@ -209,14 +209,13 @@ const config = {
       }
     },
     {
-      id: INVENTORY_TYPES.INVENTORY_ID,
-      cellWidth: 40
+      metric: INVENTORY_TYPES.INVENTORY_ID,
+      width: 40
     },
     {
-      id: INVENTORY_TYPES.LAST_SEEN,
-      cell: ({ [INVENTORY_TYPES.LAST_SEEN]: lastSeen } = {}) =>
-        (lastSeen?.value && <DateFormat date={lastSeen?.value} />) || '',
-      cellWidth: 15
+      metric: INVENTORY_TYPES.LAST_SEEN,
+      cell: ({ [INVENTORY_TYPES.LAST_SEEN]: lastSeen } = {}) => (lastSeen && <DateFormat date={lastSeen} />) || '',
+      width: 15
     }
   ],
   initialInventoryFilters: [
