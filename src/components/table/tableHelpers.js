a162608cccbbba79dfaf68b3e3474c0c2d2059d2
CD Cabrera <cabrera.code@gmail.com>
2023-09-11 12:42:52 -0400

    DEV clean up

--- src/components/table/tableHelpers.js
+++ src/components/table/tableHelpers.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { SortByDirection } from '@patternfly/react-table';
-// import _cloneDeep from 'lodash/cloneDeep';
-// import { helpers } from '../../common/helpers';
 
 /**
  * Table sorting directions.
