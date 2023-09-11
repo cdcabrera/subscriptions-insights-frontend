1d2009ce44a17ed32d2be2e3f326654f1fb40987
CD Cabrera <cabrera.code@gmail.com>
2023-09-11 13:55:14 -0400

    DEV inventoryCardInstances test

---
+++ src/components/inventoryCardInstances/__tests__/inventoryCardInstances.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { InventoryCardInstances } from '../inventoryCardInstances';
+import { RHSM_API_QUERY_SET_TYPES } from '../../../services/rhsm/rhsmConstants';
+
+describe('InventoryCardInstances Component', () => {
+  it('should render a basic component', async () => {
+    const props = {
+      useProductInventoryConfig: () => ({ filters: [], settings: {} }),
+      useProductInventoryQuery: () => ({
+        [RHSM_API_QUERY_SET_TYPES.LIMIT]: 10,
+        [RHSM_API_QUERY_SET_TYPES.OFFSET]: 0
+      })
+    };
+
+    const component = await shallowComponent(<InventoryCardInstances {...props} />);
+    expect(component).toMatchSnapshot('basic render');
+  });
+});
