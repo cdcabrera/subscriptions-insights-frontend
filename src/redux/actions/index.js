5bbed9e24d4697404e757c1743a498840574911d
CD Cabrera <cabrera.code@gmail.com>
2024-01-17 20:23:59 -0500

    DEV incremental remove unused actions

--- src/redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-import { platformActions } from './platformActions';
-
-const actions = {
-  platform: platformActions
-};
-
-const reduxActions = { ...actions };
-
-export { reduxActions as default, reduxActions, platformActions };
