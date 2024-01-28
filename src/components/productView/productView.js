e15006ec8814e916d639a7e954e14af9d9e29f88
CD Cabrera <cabrera.code@gmail.com>
2024-01-28 10:27:58 -0500

    refactor(productView): simplify, unnecessary hook

--- src/components/productView/productView.js
+++ src/components/productView/productView.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { routerContext } from '../router';
 import { ProductViewContext } from './productViewContext';
@@ -35,55 +35,51 @@ import { translate } from '../i18n/i18n';
 const ProductView = ({ t, useRouteDetail: useAliasRouteDetail }) => {
   const { firstMatch, productGroup } = useAliasRouteDetail();
 
-  const renderProduct = useCallback(() => {
-    const updated = config => {
-      const { initialInventoryFilters, initialSubscriptionsInventoryFilters, productId, viewId } = config;
+  const renderContent = () => {
+    const { initialInventoryFilters, initialSubscriptionsInventoryFilters, productId, viewId } = firstMatch;
 
-      if (!productId || !viewId) {
-        return null;
-      }
+    if (!productId || !viewId) {
+      return null;
+    }
 
-      return (
-        <ProductViewContext.Provider value={config} key={`product_${productId}`}>
-          <PageMessages>
-            <BannerMessages />
-          </PageMessages>
-          <PageToolbar>
-            <Toolbar />
-          </PageToolbar>
-          <PageSection className="curiosity-page-section__graphs">
-            <GraphCard />
-          </PageSection>
-          <PageSection className="curiosity-page-section__tabs">
-            <InventoryTabs
-              isDisabled={
-                (!initialInventoryFilters && !initialSubscriptionsInventoryFilters) || helpers.UI_DISABLED_TABLE
-              }
-            >
-              {!helpers.UI_DISABLED_TABLE_INSTANCES && initialInventoryFilters && (
-                <InventoryTab
-                  key={`inventory_instances_${productId}`}
-                  title={t('curiosity-inventory.tabInstances', { context: [productId] })}
-                >
-                  <InventoryCardInstances />
-                </InventoryTab>
-              )}
-              {!helpers.UI_DISABLED_TABLE_SUBSCRIPTIONS && initialSubscriptionsInventoryFilters && (
-                <InventoryTab
-                  key={`inventory_subs_${productId}`}
-                  title={t('curiosity-inventory.tabSubscriptions', { context: [productId] })}
-                >
-                  <InventoryCardSubscriptions />
-                </InventoryTab>
-              )}
-            </InventoryTabs>
-          </PageSection>
-        </ProductViewContext.Provider>
-      );
-    };
-
-    return updated(firstMatch);
-  }, [firstMatch, t]);
+    return (
+      <ProductViewContext.Provider value={firstMatch}>
+        <PageMessages>
+          <BannerMessages />
+        </PageMessages>
+        <PageToolbar>
+          <Toolbar />
+        </PageToolbar>
+        <PageSection className="curiosity-page-section__graphs">
+          <GraphCard />
+        </PageSection>
+        <PageSection className="curiosity-page-section__tabs">
+          <InventoryTabs
+            isDisabled={
+              (!initialInventoryFilters && !initialSubscriptionsInventoryFilters) || helpers.UI_DISABLED_TABLE
+            }
+          >
+            {!helpers.UI_DISABLED_TABLE_INSTANCES && initialInventoryFilters && (
+              <InventoryTab
+                key={`inventory_instances_${productId}`}
+                title={t('curiosity-inventory.tabInstances', { context: [productId] })}
+              >
+                <InventoryCardInstances />
+              </InventoryTab>
+            )}
+            {!helpers.UI_DISABLED_TABLE_SUBSCRIPTIONS && initialSubscriptionsInventoryFilters && (
+              <InventoryTab
+                key={`inventory_subs_${productId}`}
+                title={t('curiosity-inventory.tabSubscriptions', { context: [productId] })}
+              >
+                <InventoryCardSubscriptions />
+              </InventoryTab>
+            )}
+          </InventoryTabs>
+        </PageSection>
+      </ProductViewContext.Provider>
+    );
+  };
 
   return (
     (productGroup && (
@@ -91,7 +87,7 @@ const ProductView = ({ t, useRouteDetail: useAliasRouteDetail }) => {
         <PageHeader productLabel={productGroup}>
           {t(`curiosity-view.title`, { appName: helpers.UI_DISPLAY_NAME, context: productGroup })}
         </PageHeader>
-        <PageColumns>{renderProduct()}</PageColumns>
+        <PageColumns>{renderContent()}</PageColumns>
       </PageLayout>
     )) ||
     null
