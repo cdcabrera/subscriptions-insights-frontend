c9114a53eadfbfc9dd55f2dc0d34d57bc0192531
CD Cabrera <cabrera.code@gmail.com>
2024-02-14 10:23:21 -0500

    refactor(services): sw-2024 refine export response transformers

--- src/services/platform/platformTransformers.js
+++ src/services/platform/platformTransformers.js
@@ -1,27 +1,106 @@
 import { rbacConfig } from '../../config';
 import {
   platformConstants,
+  PLATFORM_API_EXPORT_STATUS_TYPES,
+  PLATFORM_API_EXPORT_FILENAME_PREFIX as EXPORT_PREFIX,
   PLATFORM_API_RESPONSE_USER_PERMISSION_OPERATION_TYPES as OPERATION_TYPES,
   PLATFORM_API_RESPONSE_USER_PERMISSION_RESOURCE_TYPES as RESOURCE_TYPES
 } from './platformConstants';
 import { helpers } from '../../common';
 
 /**
- * Transform platform responses. Replaces selector usage.
+ * Transform export responses. Combines multiple exports, or a single export,
+ * into the same response format.
  *
  * @memberof Platform
  * @module PlatformTransformers
  */
 
+/**
+ * Parse platform export response.
+ *
+ * @param {object} response
+ * @returns {object}
+ */
 const exports = response => {
-  const updatedResponse = {};
+  const updatedResponse = { data: {}, meta: {} };
   const {
+    [platformConstants.PLATFORM_API_EXPORT_RESPONSE_DATA]: data,
+    [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.FORMAT]: format,
     [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.ID]: id,
+    [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.NAME]: name,
     [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.STATUS]: status
   } = response || {};
 
-  updatedResponse.id = id;
-  updatedResponse.status = status;
+  const getProductId = str => {
+    const updatedStr = str;
+    const attemptId = updatedStr?.replace(`${EXPORT_PREFIX}-`, '')?.trim();
+
+    if (attemptId === updatedStr) {
+      return undefined;
+    }
+
+    return attemptId;
+  };
+
+  const getProductStatus = str => {
+    const updatedStr = str;
+    let updatedStatus = PLATFORM_API_EXPORT_STATUS_TYPES.PENDING;
+
+    if (
+      updatedStr === PLATFORM_API_EXPORT_STATUS_TYPES.FAILED ||
+      updatedStr === PLATFORM_API_EXPORT_STATUS_TYPES.COMPLETED
+    ) {
+      updatedStatus = updatedStr;
+    }
+
+    return updatedStatus;
+  };
+
+  if (Array.isArray(data)) {
+    data
+      .filter(({ [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.NAME]: exportName }) =>
+        new RegExp(`^${EXPORT_PREFIX}`, 'i').test(exportName)
+      )
+      .forEach(
+        ({
+          [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.FORMAT]: exportFormat,
+          [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.ID]: exportId,
+          [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.NAME]: exportName,
+          [platformConstants.PLATFORM_API_EXPORT_RESPONSE_TYPES.STATUS]: exportStatus
+        }) => {
+          const productId = getProductId(exportName);
+          const focusedStatus = getProductStatus(exportStatus);
+
+          if (updatedResponse.data.isAnythingPending !== true) {
+            updatedResponse.data.isAnythingPending = focusedStatus === PLATFORM_API_EXPORT_STATUS_TYPES.PENDING;
+          }
+
+          updatedResponse.data[productId] ??= [];
+          updatedResponse.data[productId].push({
+            format: exportFormat,
+            id: exportId,
+            name: exportName,
+            status: focusedStatus
+          });
+        }
+      );
+  } else if (id && status && new RegExp(`^${EXPORT_PREFIX}`, 'i').test(name)) {
+    const productId = getProductId(name);
+    const focusedStatus = getProductStatus(status);
+
+    if (updatedResponse.data.isAnythingPending !== true) {
+      updatedResponse.data.isAnythingPending = focusedStatus === PLATFORM_API_EXPORT_STATUS_TYPES.PENDING;
+    }
+
+    updatedResponse.data[productId] ??= [];
+    updatedResponse.data[productId].push({
+      format,
+      id,
+      name,
+      status: focusedStatus
+    });
+  }
 
   return updatedResponse;
 };
