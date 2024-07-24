e8323fabefba53ff6d93bc25f211df5217ce776c
CD Cabrera <cabrera.code@gmail.com>
2024-07-24 13:13:31 -0400

    fix(toolbarFieldExport): sw-117 apply export label
    * toolbarFieldExport, apply export label copy
    * toolbar, standalone single item filters without padding
    * select, allow plain dropdown button

--- src/components/toolbar/toolbarFieldExport.js
+++ src/components/toolbar/toolbarFieldExport.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { ExportIcon } from '@patternfly/react-icons';
 import { useProduct, useProductExportQuery } from '../productView/productViewContext';
 import { useExport, useExistingExports, useExportStatus } from './toolbarFieldExportContext';
-import { Select, SelectPosition, SelectButtonVariant } from '../form/select';
+import { Select, SelectPosition, SelectButtonVariant, SplitButtonVariant } from '../form/select';
 import {
   PLATFORM_API_EXPORT_APPLICATION_TYPES as APP_TYPES,
   PLATFORM_API_EXPORT_CONTENT_TYPES as FIELD_TYPES,
@@ -129,8 +129,13 @@ const ToolbarFieldExport = ({
       placeholder={t('curiosity-toolbar.placeholder', { context: 'export' })}
       position={position}
       data-test="toolbarFieldExport"
-      toggleIcon={<ExportIcon />}
-      buttonVariant={SelectButtonVariant.plain}
+      buttonVariant={SelectButtonVariant.secondary}
+      splitButtonVariant={SplitButtonVariant.default}
+      splitButtonContent={
+        <React.Fragment>
+          <ExportIcon /> {t('curiosity-toolbar.label', { context: 'export' })}
+        </React.Fragment>
+      }
     />
   );
 };
